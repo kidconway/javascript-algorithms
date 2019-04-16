6cd59f8c5e282179d9734ff55ce2a5fa01b2e505
Colby Horan <colbyhoran@gmail.com>
2019-04-16 14:54:45 -0500

    update to sandbox, nothing major

--- Masterclass/sandbox.js
+++ Masterclass/sandbox.js
@@ -14,14 +14,22 @@ const doubleArray = (x) => {
   x.forEach((item) => { resultArr.push(item * 2) })
   return resultArr
 }
+
+
 const characterCount = (str) => {
   let obj = {}
-  for (let char of str){
-    char = char.toLowerCase(str)
-    if (!obj[char]) {
-      obj[char] = 1
-    } else {
-      obj[char]++
+  str = str.replace(/[^0-9A-Z]+/gi,"")
+  if (typeof str !== 'string') {
+    console.log('Invalid Input')
+  } else {
+    console.log('else')
+    for (let char of str){
+      char = char.toLowerCase(str)
+      if (!obj[char] && typeof 'string') {
+        obj[char] = 1
+      } else if (typeof 'string') {
+        obj[char]++
+      }
     }
   }
   return obj
@@ -36,27 +44,31 @@ const characterCount = (str) => {
 
 
 // Declarations
-let arr = [1, 2, 3, 4, 5]
+let arr = [4, 8, 15, 16, 23, 42]
 let newArr = []
-let str = 'I order the club sandwich all the time. And I\'m not even a member, man. I don\'t know how I get away with it.'
+let str1 = 'I order the club sandwich all the time. And I\'m not even a member, man. I don\'t know how I get away with it.'
+let str2 = "Al Michaels: You're excited?!? Feel these nipples!!"
+
+// console.log('\n')
+// console.log(addItUp(10));
+//
+// console.log('\n')
+// arr.forEach((x) => {newArr.push(x * 45)})
+// console.log(arr)
+// console.log(newArr)
+//
+// console.log('\n')
+// console.log(multiplyByTwo(10))
+// console.log(multiplyByThree(10))
+//
+// console.log('\n')
+// console.log(doubleArray([1,2,3]))
 
 console.log('\n')
-console.log(addItUp(10));
+console.log(str1)
+console.log(characterCount(str1))
 
-console.log('\n')
-arr.forEach((x) => {newArr.push(x * 45)})
-console.log(arr)
-console.log(newArr)
 
-console.log('\n')
-console.log(multiplyByTwo(10))
-console.log(multiplyByThree(10))
-
-console.log('\n')
-console.log(doubleArray([1,2,3]))
-
-console.log('\n')
-console.log(characterCount(str))
 
 console.log('\n \n My favorite quote:')
 console.log('"George Bush doesn\'t care about black people." \n - Pyschotic Legend Kanye West\n')
